abe4b6f12ee888d60d547cf0bb0b5f6966e29cb5
Harsh <goyalh4164@gmail.com>
2023-06-03 20:46:31 +0530

    Attached mongoose cloud for easy future deployment

---
+++ data/database.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const connectDB = ()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName :"ThoughtFlow"
+    }).then((c)=>console.log(`Database Connected with ${c.connection.host}`))
+    .catch((e) => console.log(e));
+}
