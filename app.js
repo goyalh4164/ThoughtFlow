bc88604547efc35802dd24bbd2e3b486e82df87b
Harsh <goyalh4164@gmail.com>
2023-06-04 18:42:06 +0530

    modified the cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin : [process.env.FRONTEND],
+    origin : [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
