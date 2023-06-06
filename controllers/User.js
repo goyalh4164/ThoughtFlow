869c65e31c6819bca1f239119f61bf17c3a36c7a
Harsh <goyalh4164@gmail.com>
2023-06-06 13:36:02 +0530

    modified the logout function

--- controllers/User.js
+++ controllers/User.js
@@ -96,8 +96,11 @@ export const deleteUserAccount = async (req, res) => {
 
 export const logoutUserAccount = async (req, res) => {
   try {
-    // Clear the token cookie
-    res.clearCookie('token');
+    // Clear the token cookie by setting it to an empty string and an expired date
+    res.cookie('token', '', {
+      httpOnly: true,
+      expires: new Date(0),
+    });
 
     res.json({ message: 'User logged out successfully.' });
   } catch (error) {
